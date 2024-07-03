d3fa8179f9719477e62a8d3c867052638890f538
tangzhiyi11 <tangzhiyi@pjlab.org.cn>
2024-07-03 08:44:33 +0000

    fix fill_kv_cache

--- lmdeploy/pytorch/kernels/muxi/fill_kv_cache.py
+++ lmdeploy/pytorch/kernels/muxi/fill_kv_cache.py
@@ -8,26 +8,15 @@ def fill_kv_cache(k_states: Tensor, v_states: Tensor, k_caches: Tensor,
                   v_caches: Tensor, q_start_loc: Tensor, q_seq_length: Tensor,
                   kv_seq_length: Tensor, max_q_seq_length: int,
                   block_offsets: Tensor, context: None):
-    block_num, head, block_size, dim = v_caches.size()
-    x = 32 // k_caches.element_size()
-
+    # x = 32 // k_caches.element_size()
+    # k_caches shape: [block_num, kv_head_num, head_size // x, block_size, x]
+    # v_caches shape: [block_num, kv_head_num, block_size, head_size]
     vllm_ops.cache_ops.reshape_and_cache_new(
         k_states,
-        v_states,
+        v_states.contiguous(),
         k_caches,
         v_caches,
         context.kv_start_indices,
         'auto',
     )
-    lm_v_caches = v_caches.transpose(2, 1).contiguous()
-    dest_index_copy_kv(v_states, context.kv_start_indices, lm_v_caches)
-    lm_v_caches = lm_v_caches.transpose(2, 1).contiguous()
-    v_caches[:] = lm_v_caches
     return
-
-
-def dest_index_copy_kv(states, dest_loc, caches):
-    block_num, block_size, head, dim = caches.size()
-    caches_tmp = caches.view(block_num * block_size, head, dim)
-    caches_tmp[dest_loc] = states
-    caches[:] = caches_tmp.view(block_num, block_size, head, dim)

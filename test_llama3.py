fix fused_rotary_emb precision.

test_llama3.py
@@ -4,8 +4,8 @@ import lmdeploy
 from lmdeploy import PytorchEngineConfig
 
 if __name__ == "__main__":
-    # torch.manual_seed(10)
-    # random.seed(10)
+    torch.manual_seed(10)
+    random.seed(10)
     pipe = lmdeploy.pipeline("/home/SAIL/zhousl/Meta-Llama-3-8B-Instruct",
                             backend_config = PytorchEngineConfig(tp=2,
                                                                  block_size=16,
6046c1b31721005d4b513bc3e98370e0b654c3c2
zhoushenglong <zhoushenglong@pjlab.org.cn>
2024-07-07 22:48:41 +0000

    add mixtral test.

---
+++ test_mixtral.py
@@ -0,0 +1,20 @@
+import torch
+import random
+import lmdeploy
+from lmdeploy import PytorchEngineConfig
+
+if __name__ == "__main__":
+    torch.manual_seed(10)
+    random.seed(10)
+    pipe = lmdeploy.pipeline("/home/SAIL/zhousl/Mixtral-8x7B-Instruct-v0___1",
+                            backend_config = PytorchEngineConfig(tp=4,
+                                                                 block_size=16,
+                                                                 device_type='muxi',
+                                                                 cache_max_entry_count=0.4))
+    # question = ["How are you?", "Please introduce China.", "Introduce Shanghai AI Lab"]
+    question = ["Shanghai is"]
+    response = pipe(question, do_preprocess=True)
+    for idx, r in enumerate(response):
+        print(f"Q: {question[idx]}")
+        print(f"A: {r.text}")
+        print()

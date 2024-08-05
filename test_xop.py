4ae1787eb94a16aa65e7cb319ebb2519582a7aad
zhoushenglong <1768552509@qq.com>
2024-08-05 17:41:30 +0800

    add code.

--- test_xop.py
+++ test_xop.py
@@ -1,18 +1,27 @@
 import torch
-import xformers.ops as xops
+# import xformers.ops as xops
+
+from torch.nn import functional as F
 
 if __name__ == "__main__":
-    q = torch.randn(1, 1226, 16, 112, dtype=torch.float16)
-    k = torch.randn(1, 1226, 16, 112, dtype=torch.float16)
-    v = torch.randn(1, 1226, 16, 112, dtype=torch.float16)
+    query = torch.randn(1, 1226, 16, 112, dtype=torch.float16).cuda().contiguous()
+    key = torch.randn(1, 1226, 16, 112, dtype=torch.float16).cuda().contiguous()
+    value = torch.randn(1, 1226, 16, 112, dtype=torch.float16).cuda().contiguous()
     scale = 0.94
 
+    # import pdb; pdb.set_trace()
+
+    # out = xops.memory_efficient_attention(
+    #     query, key, value, scale=scale,
+    # )
+
     import pdb; pdb.set_trace()
 
-    out = xops.memory_efficient_attention(
-        q, k, v, scale=scale,
-    )
+    scale = 1 / query.shape[-1] ** 0.5
+    query = query * scale
+    attn = query @ key.transpose(-2, -1)
+    attn = attn.softmax(-1)
+    attn = F.dropout(attn, 0.0)
+    res = attn @ value
 
     import pdb; pdb.set_trace()
-
-    

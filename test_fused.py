c1a36eaf6135234b6a7190e377fea15bac3a0216
zhoushenglong <zhoushenglong@pjlab.org.cn>
2024-08-06 07:47:20 +0000

    add code.

--- test_fused.py
+++ test_fused.py
@@ -4,15 +4,16 @@ from vllm._C import ops
 from torch.nn import functional as F
 
 if __name__ == "__main__":
-    # seq_len = 47
-    seq_len = 1235
-    position_ids = torch.range(0, seq_len - 1).reshape(seq_len, 1).cuda()
+    seq_len = 47
+    seq_len = 1021
+    # seq_len = 1235
+    position_ids = torch.range(0, seq_len - 1, dtype=torch.int64).reshape(seq_len, 1).cuda()
     query_states = torch.randn(1, seq_len, 4096, dtype=torch.bfloat16).cuda()
     key_states = torch.randn(1, seq_len, 4096, dtype=torch.bfloat16).cuda()
     cos_sin_cache = torch.randn(1, seq_len, 128, dtype=torch.bfloat16).cuda()
     head_dim = 128
     
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     ops.rotary_embedding(position_ids,
                         query_states,
                         key_states,
@@ -21,3 +22,6 @@ if __name__ == "__main__":
                         True)
 
     import pdb; pdb.set_trace()
+
+    print(query_states.shape, flush=True)
+    print("Run done.", flush=True)

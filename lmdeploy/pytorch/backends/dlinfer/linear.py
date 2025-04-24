fab76009051c536d3065fd8118fbde4e2bdc3347
yaofengchen <fengchenyao@foxmail.com>
2025-04-24 01:17:38 +0000

    ascend eager mode dp

--- lmdeploy/pytorch/backends/dlinfer/linear.py
+++ lmdeploy/pytorch/backends/dlinfer/linear.py
@@ -8,7 +8,35 @@ import lmdeploy.pytorch.distributed as dist
 from lmdeploy.pytorch.kernels.dlinfer import linear
 
 from ..linear import LinearBuilder, LinearImpl
+from torch import distributed as torch_dist
+# from ..default.linear import _reduce_scatter_input
 
+
+def _reduce_scatter_input(out: torch.Tensor, rank: int, tp_sizes: List[int]):
+    """reduce scatter."""
+    out = out.transpose(0, -2)
+    if not out.is_contiguous():
+        out = out.contiguous()
+    outs = out.split(tp_sizes, 0)
+    out = outs[rank]
+    outs = list(outs)
+    # dist.reduce_scatter(out, outs)
+    # reduce
+    for tensor in outs:
+        dist.all_reduce(tensor,  op=dist.ReduceOp.SUM)
+    
+    # scatter
+    combined = torch.cat(outs)
+    world_size = torch_dist.get_world_size() 
+    chunk_size = combined.shape[0]  // world_size 
+    start = rank * chunk_size 
+    end = start + chunk_size if rank != world_size - 1 else combined.shape[0] 
+    
+    # copy
+    out.copy_(combined[start:end])
+
+    out = out.transpose(0, -2)
+    return out
 
 class DlinferLinearImpl(LinearImpl):
     """Dlinfer linear implementation api."""
@@ -29,7 +57,10 @@ class DlinferLinearImpl(LinearImpl):
         """forward."""
         out = linear(x, weight, bias, False)
         if all_reduce:
-            dist.all_reduce(out)
+            if scatter_size is not None:
+                out = _reduce_scatter_input(out, rank, scatter_size)
+            else:
+                dist.all_reduce(out)
         return out
 
 
